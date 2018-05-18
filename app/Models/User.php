f3d12d8f708e393448dd0514e3204bc613e65470
vagrant <vagrant@homestead>
2018-05-18 07:24:37 +0000

    List topics created by user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Topic;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -25,4 +26,8 @@ class User extends Authenticatable {
 	protected $hidden = [
 		'password', 'remember_token',
 	];
+
+	public function topics() {
+		return $this->hasMany(Topic::class);
+	}
 }
