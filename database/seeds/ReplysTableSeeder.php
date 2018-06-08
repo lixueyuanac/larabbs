70494b146c151bd5035148e719cee51288f042e6
vagrant <vagrant@homestead>
2018-06-08 03:10:52 +0000

    List user replies

---
+++ database/seeds/ReplysTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\Reply;
+use App\Models\Topic;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class ReplysTableSeeder extends Seeder {
+	public function run() {
+		//所有用户id
+		$user_ids = User::all()->pluck('id')->toArray();
+		//所有话题id数组
+		$topic_ids = Topic::all()->pluck('id')->toArray();
+		//获取faker势力
+		$faker = app(Faker\Generator::class);
+
+		$replys = factory(Reply::class)->times(1000)->make()->each(function ($reply, $index) use ($user_ids, $topic_ids, $faker) {
+			// 从用户 ID 数组中随机取出一个并赋值
+			$reply->user_id = $faker->randomElement($user_ids);
+
+			// 话题 ID，同上
+			$reply->topic_id = $faker->randomElement($topic_ids);
+		});
+
+		Reply::insert($replys->toArray());
+	}
+
+}
