1d131586e08efd614c2bd814b89a0886606e7686
vagrant <vagrant@homestead>
2018-06-15 07:28:08 +0000

    add dingo

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,18 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function ($api) {
+	$api->get('version', function () {
+		return response('this is version v1');
+	});
+});
+
+$api->version('v2', function ($api) {
+	$api->get('version', function () {
+		return response('this is version v2');
+	});
 });
