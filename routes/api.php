ffadb2cf9181ad98f890b3c549e1f1814968a6bb
vagrant <vagrant@homestead>
2018-06-21 08:23:09 +0000

    verificationCode store

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
  */
 
+Route::get('aa', function () {
+	echo 'aa';
+});
+
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
@@ -25,4 +29,12 @@ $api->version('v2', function ($api) {
 	$api->get('version', function () {
 		return response('this is version v2');
 	});
+});
+
+$api->version('v1', [
+	'namespace' => 'App\Http\Controllers\Api',
+], function ($api) {
+	// 短信验证码
+	$api->post('verificationCodes', 'VerificationCodesController@store')
+		->name('api.verificationCodes.store');
 });
